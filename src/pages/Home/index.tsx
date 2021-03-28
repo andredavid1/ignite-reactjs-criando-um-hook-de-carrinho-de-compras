774ff79084006210b6e55ad84cbb8bf81d86342a
André David Santos <andredavid1@yahoo.com.br>
2021-03-28 16:51:49 -0300

    feat: start cart with localstorage values

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -17,13 +17,13 @@ interface ProductFormatted extends Product {
   priceFormatted: string;
 }
 
-interface CartItemsAmount {
-  [key: number]: number;
-}
+// interface CartItemsAmount {
+//   [key: number]: number;
+// }
 
 const Home = (): JSX.Element => {
   const [products, setProducts] = useState<ProductFormatted[]>([]);
-  // const { addProduct, cart } = useCart();
+  const { addProduct, cart } = useCart();
 
   // const cartItemsAmount = cart.reduce((sumAmount, product) => {
   //   // TODO
@@ -52,25 +52,26 @@ const Home = (): JSX.Element => {
 
   return (
     <ProductList>
-        {products && products.map(product => (
-          <li key={product.id}>
-            <img src={product.image} alt={product.title} />
-            <strong>{product.title}</strong>
-            <span>{product.priceFormatted}</span>
-            <button
-              type="button"
-              data-testid="add-product-button"
-              onClick={() => handleAddProduct(product.id)}
-            >
-              <div data-testid="cart-product-quantity">
-                <MdAddShoppingCart size={16} color="#FFF" />
-                {/* {cartItemsAmount[product.id] || 0} */} 2
-              </div>
+      {console.log(cart)}  
+      {products && products.map(product => (
+        <li key={product.id}>
+          <img src={product.image} alt={product.title} />
+          <strong>{product.title}</strong>
+          <span>{product.priceFormatted}</span>
+          <button
+            type="button"
+            data-testid="add-product-button"
+            onClick={() => handleAddProduct(product.id)}
+          >
+            <div data-testid="cart-product-quantity">
+              <MdAddShoppingCart size={16} color="#FFF" />
+              {/* {cartItemsAmount[product.id] || 0} */} 2
+            </div>
 
-              <span>ADICIONAR AO CARRINHO</span>
-            </button>
-          </li>
-        ))}        
+            <span>ADICIONAR AO CARRINHO</span>
+          </button>
+        </li>
+      ))}        
     </ProductList>
   );
 };
